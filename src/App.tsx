ae3b02fae13a586102a8a994ec27f586e6b7aa71
odevthomas <developer.thomas@outlook.com.br>
2025-07-03 14:21:49 -0300

    vite

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* Passa o basename aqui */}
-      <BrowserRouter basename="/vendas">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
