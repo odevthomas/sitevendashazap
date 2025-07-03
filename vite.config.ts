e22a4abfcd63403f8b7be4da5995a07981c52117
odevthomas <developer.thomas@outlook.com.br>
2025-07-03 15:14:16 -0300

    teste vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/vendas/",  // ESSENCIAL para rodar no subdiretório /vendas
+
   server: {
-    host: "::",
+    host: true,       // Permite acessar via IP/local
     port: 8080,
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
