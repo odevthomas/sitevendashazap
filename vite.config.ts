544138bf73c45bc0a3de150115cd464f6cee8c7c
odevthomas <developer.thomas@outlook.com.br>
2025-07-03 13:59:35 -0300

    push

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/vendas/', // 👈 isso aqui é ESSENCIAL
   server: {
     host: "::",
     port: 8080,
